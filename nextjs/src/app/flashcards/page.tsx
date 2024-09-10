53aa74698a54366ad8a245d3a78053f0f01bd3ac
Kristian <49764796+kristianka@users.noreply.github.com>
2024-09-10 10:38:06 +0300

    init flash cards

--- nextjs/src/app/flashcards/page.tsx
+++ nextjs/src/app/flashcards/page.tsx
@@ -1,10 +1,16 @@
 import FlashCard from "@/components/flashcard";
+import Link from "next/link";
 
 export default function Home() {
     return (
         <div>
+            <h1 className="text-center text-3xl mt-10">Flash cards</h1>
+            <div className="text-center mb-10">
+                <Link href="/" className="font-bold">
+                    Home
+                </Link>
+            </div>
             <FlashCard />
-            <p>hello world from flashcards</p>
         </div>
     );
 }
--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
     return (
-        <main>
-            <p>hello world</p>
+        <main className="">
+            <h1 className="text-center text-3xl mt-10">Home</h1>
+            <div className="text-center mb-10">
+                <Link href="/flashcards" className="font-bold">
+                    Flash cards
+                </Link>
+            </div>
         </main>
     );
 }
