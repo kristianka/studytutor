8494b00aa46c6c694ec0c6e28cacd6766e0de4f3
Kristian <49764796+kristianka@users.noreply.github.com>
2024-09-11 19:12:05 +0300

    create layout for flash cards front page, add shadcn

--- nextjs/src/app/flashcards/page.tsx
+++ nextjs/src/app/flashcards/page.tsx
@@ -2,6 +2,12 @@ import FlashCard from "@/components/flashcard";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import CardOptions from "@/components/flashcards/Card";
+import History from "@/components/flashcards/History";
+import { Button } from "@/components/ui/button";
+
 export default async function Home() {
     const supabase = createClient();
 
@@ -11,9 +17,38 @@ export default async function Home() {
     }
 
     return (
-        <div>
-            <FlashCard />
-            <p>hello world from flashcards</p>
+        <div className="">
+            <div className="flex justify-between items-center">
+                <div className="">
+                    <h1 className="text-3xl font-bold">Hi, Einari Nari!</h1>
+                    <h2 className="text-xl font-bold">What do you want to learn about today? 😊</h2>
+                </div>
+                <h2 className="text-2xl font-bold">🔥21</h2>
+            </div>
+            <div className="mt-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-y-10 sm:gap-y-0 gap-x-10">
+                <div className="md:col-span-3 max-w-2xl">
+                    <Label htmlFor="topic">Please enter a topic</Label>
+                    <Input type="topic" id="topic" placeholder="React hooks" />
+                    <div className="my-5">
+                        <CardOptions />
+                    </div>
+                    <p className="block md:hidden mx-3 text-sm text-muted-foreground">
+                        Tip: Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+                        numquam molestiae sequi.
+                    </p>
+                    <div className="flex justify-end gap-x-5">
+                        <p className="hidden md:block mx-3 text-sm text-muted-foreground">
+                            Tip: Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+                            numquam molestiae sequi.
+                        </p>
+                        <Button className="items-end">Reset</Button>
+                        <Button className="items-end">Start</Button>
+                    </div>
+                </div>
+                <div className="md:col-span-2">
+                    <History />
+                </div>
+            </div>
         </div>
     );
 }
--- nextjs/src/components/NavBar.tsx
+++ nextjs/src/components/NavBar.tsx
@@ -8,7 +8,7 @@ export default async function NavBar() {
 
     return (
         <header className="flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-3 dark:bg-neutral-800">
-            <nav className="max-w-7xl w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
+            <nav className="max-w-7xl w-full mx-auto sm:flex sm:items-center sm:justify-between">
                 <Link
                     className="flex-none font-semibold text-xl text-black focus:outline-none focus:opacity-80 dark:text-white"
                     href="/"
