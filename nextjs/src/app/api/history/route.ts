34bcfee801ea46942c47bf60a6e230642ae01d60
Kristian <49764796+kristianka@users.noreply.github.com>
2024-09-24 00:12:43 +0300

    fetch flash cards history on flash cards page

---
+++ nextjs/src/app/api/history/route.ts
@@ -0,0 +1,145 @@
+"use server";
+import { createClient } from "@/utils/supabase/server";
+import { OpenAI } from "openai";
+import { NextResponse } from "next/server";
+
+async function initClients() {
+    try {
+        const supabase = createClient();
+        const openai = new OpenAI({
+            apiKey: process.env.OPENAI_API_KEY
+        });
+        return { supabase, openai };
+    } catch (error) {
+        console.error("Error initializing clients:", error);
+        throw new Error("Failed to initialize clients");
+    }
+}
+
+// retrieve assistant from database
+async function getAssistant(supabase: ReturnType<typeof createClient>) {
+    const { data, error } = await supabase
+        .from("assistants")
+        .select("*")
+        .eq("name", "StudyHelper")
+        .single();
+
+    if (error) {
+        console.error("Error fetching assistant:", error);
+        throw new Error("Assistant not found");
+    }
+
+    return data;
+}
+
+// create thread and store it in database
+async function getOrCreateThread(
+    supabase: ReturnType<typeof createClient>,
+    supabaseUserId: string,
+    assistantId: string
+) {
+    const { data: profile, error: profileError } = await supabase
+        .from("profiles")
+        .select("id, thread_id")
+        .eq("id", supabaseUserId)
+        .maybeSingle();
+
+    // error fetching profile
+    if (profileError) {
+        console.error("Error fetching profile:", profileError);
+        throw new Error("Error fetching profile data");
+    }
+
+    // no profile id found
+    if (!profile) {
+        console.error("Profile not found with Supabase User ID:", supabaseUserId);
+        throw new Error("Profile not found");
+    }
+
+    // check if thread exists
+    if (profile.thread_id) {
+        const { data: existingThread, error: threadCheckError } = await supabase
+            .from("threads")
+            .select("id")
+            .eq("id", profile.thread_id)
+            .maybeSingle();
+
+        if (threadCheckError) {
+            console.error("Error checking thread existence:", threadCheckError);
+            throw new Error("Error checking thread existence");
+        }
+
+        if (existingThread) {
+            console.log("Found existing thread:", profile.thread_id);
+            return profile.thread_id;
+        } else {
+            console.warn(
+                "Thread ID in profile does not exist in threads table. Clearing invalid thread_id."
+            );
+
+            await supabase.from("profiles").update({ thread_id: null }).eq("id", profile.id);
+        }
+    }
+
+    // create new thread
+    const { data: newThread, error: threadError } = await supabase
+        .from("threads")
+        .insert([{ user_id: profile.id, assistant_id: assistantId }])
+        .select()
+        .single();
+
+    if (threadError) {
+        console.error("Error creating thread:", threadError);
+        throw new Error("Failed to create new thread");
+    }
+
+    const { error: updateError } = await supabase
+        .from("profiles")
+        .update({ thread_id: newThread.id })
+        .eq("id", profile.id);
+
+    if (updateError) {
+        console.error("Error updating profile's thread_id:", updateError);
+        throw new Error("Failed to associate profile with new thread");
+    }
+
+    return newThread.id;
+}
+
+// retrieve thread history
+async function getThreadHistory(supabase: ReturnType<typeof createClient>, threadId: string) {
+    const { data, error } = await supabase
+        .from("messages")
+        .select("*")
+        .eq("thread_id", threadId)
+        .order("created_at", { ascending: true });
+
+    if (error) {
+        console.error("Error fetching thread history:", error);
+        throw new Error("Failed to retrieve thread history");
+    }
+
+    return data.map((message) => ({
+        role: message.sender,
+        content: message.message_content
+    }));
+}
+
+export async function POST(req: Request) {
+    const { userId } = await req.json();
+
+    if (!userId) {
+        return NextResponse.json({ error: "Missing userId or message" }, { status: 400 });
+    }
+
+    try {
+        const { supabase } = await initClients();
+        const assistant = await getAssistant(supabase);
+        const threadId = await getOrCreateThread(supabase, userId, assistant.id);
+        const previousMessages = await getThreadHistory(supabase, threadId);
+        return NextResponse.json({ previousMessages });
+    } catch (error) {
+        console.error("Error processing request:", error);
+        return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+    }
+}
--- nextjs/src/app/flashcards/page.tsx
+++ nextjs/src/app/flashcards/page.tsx
@@ -1,12 +1,10 @@
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
 import HistoryCard from "@/components/flashcards/HistoryCard";
-import SettingsCard from "@/components/flashcards/SettingsCard";
 import Greeting from "@/components/flashcards/Greeting";
+import NewFlashCard from "@/components/flashcards/NewFlashCard";
+import { User } from "@/types";
 
 export default async function Home() {
     const supabase = createClient();
@@ -14,7 +12,7 @@ export default async function Home() {
     if (error || !data?.user) {
         redirect("/login");
     }
-    const user = data.user;
+    const user = data.user as User;
     const firstName = user?.user_metadata.first_name;
 
     return (
@@ -22,26 +20,10 @@ export default async function Home() {
             <Greeting firstName={firstName} />
             <div className="mt-20 grid grid-cols-1 gap-x-10 gap-y-10 sm:grid-cols-2 sm:gap-y-0 md:grid-cols-5">
                 <div className="max-w-2xl md:col-span-3">
-                    <Label htmlFor="topic">Please enter a topic</Label>
-                    <Input type="topic" id="topic" placeholder="React hooks" />
-                    <div className="my-5">
-                        <SettingsCard />
-                    </div>
-                    <p className="mx-3 block text-sm text-muted-foreground md:hidden">
-                        Tip: Lorem ipsum dolor sit amet consectetur adipisicing numquam molestiae
-                        sequi.
-                    </p>
-                    <div className="flex justify-end gap-x-5">
-                        <p className="mx-3 hidden text-sm text-muted-foreground md:block">
-                            Tip: Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-                            numquam molestiae sequi.
-                        </p>
-                        <Button className="items-end">Reset</Button>
-                        <Button className="items-end">Start</Button>
-                    </div>
+                    <NewFlashCard user={user} />
                 </div>
                 <div className="md:col-span-2">
-                    <HistoryCard />
+                    <HistoryCard user={user} />
                 </div>
             </div>
         </div>
