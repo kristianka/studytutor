a068ac66fa584bc5de764e4ddadb47705f5ac175
Kristian <49764796+kristianka@users.noreply.github.com>
2024-09-30 23:42:17 +0300

    add /play route by id

--- nextjs/src/app/flashcards/play/page.tsx
+++ nextjs/src/app/flashcards/play/page.tsx
@@ -1,37 +1,26 @@
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
-import { Progress } from "@/components/ui/progress";
 import { User } from "@/types";
 import Choises from "@/components/flashcards/play/Choices";
 
-const answers = [
-    { id: 1, text: "To perform side effects" },
-    { id: 2, text: "To manage component state" },
-    { id: 3, text: "To handle component lifecycle methods" }
-];
-
-const correctAnswer = "To manage component state";
-
-export default async function Home() {
+export default async function Page({
+    searchParams
+}: {
+    searchParams: { [key: string]: string | string[] | undefined };
+}) {
     const supabase = createClient();
     const { data, error } = await supabase.auth.getUser();
     if (error || !data?.user) {
         redirect("/login");
     }
     const user = data.user as unknown as User;
-    const firstName = user?.user_metadata.first_name;
+    const session = searchParams.session as string;
+    const body = { userId: user.id, cardId: session };
 
     return (
         <div>
-            <Progress className="" value={33} />
-            <div className="mt-16 flex items-center justify-between">
-                <h2 className="text-2xl font-bold">
-                    What is the purpose of the useState hook in React?
-                </h2>
-                <h2 className="">Question 1 / 3</h2>
-            </div>
-            <Choises answers={answers} correctAnswer={correctAnswer} />
+            <Choises body={body} />
         </div>
     );
 }
--- nextjs/src/components/flashcards/play/Choices.tsx
+++ nextjs/src/components/flashcards/play/Choices.tsx
@@ -1,38 +1,70 @@
 "use client";
 import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { useState } from "react";
+import { Progress } from "@/components/ui/progress";
+import { getCards } from "@/lib/card";
+import { HistoryContent } from "@/types";
+import { useEffect, useState } from "react";
 
 interface ChoicesProps {
-    answers: { id: number; text: string }[];
-    correctAnswer: string;
+    body: { userId: string; cardId: string };
 }
 
-export default function Choices({ answers, correctAnswer }: ChoicesProps) {
+export default function Choices({ body }: ChoicesProps) {
     const [isCorrect, setIsCorrect] = useState(false);
+    const [choices, setChoices] = useState<HistoryContent | null>(null);
+    const [totalCards, setTotalCards] = useState(0);
+    const [index, setIndex] = useState(0);
 
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await getCards({ body, index });
+            const { card, totalCards } = data;
+            setChoices(card);
+            setTotalCards(totalCards);
+        };
+        void fetchData();
+    }, [body, index]);
+
+    const correctAnswer = choices?.correctAnswer;
     const selectCorrect = (value: string) => {
-        console.log(value);
         if (value !== correctAnswer) {
             return alert("Incorrect!");
         }
         setIsCorrect(true);
-        alert("Correct!");
+        // alert("Correct!");
+        setTimeout(() => {
+            setIndex((prev) => prev + 1);
+            setIsCorrect(false);
+        }, 1000);
     };
 
     return (
-        <div className="mt-32 grid grid-cols-1 gap-5 sm:grid-cols-3 sm:gap-10">
-            {answers.map((answer) => (
-                <Card
-                    key={answer.id}
-                    className={`cursor-pointer select-none hover:bg-slate-100 ${isCorrect ? "bg-green-400" : ""}`}
-                    onClick={() => selectCorrect(answer.text)}
-                >
-                    <CardHeader>
-                        <CardDescription>Choice {answer.id}</CardDescription>
-                        <CardTitle>{answer.text}</CardTitle>
-                    </CardHeader>
-                </Card>
-            ))}
+        <div>
+            <Progress className="" value={(index / totalCards) * 100} />
+            <div className="mt-16 flex items-center justify-between">
+                <h2 className="text-2xl font-bold">{choices && choices.question}</h2>
+                <h2 className="">
+                    Question {index + 1} of {totalCards}
+                </h2>
+            </div>
+
+            <div className="mt-32 grid grid-cols-1 gap-5 sm:grid-cols-3 sm:gap-10">
+                {choices &&
+                    choices.answers.map((answer) => (
+                        <Card
+                            key={answer}
+                            className={`cursor-pointer select-none hover:bg-slate-100 ${
+                                isCorrect && answer === correctAnswer ? "bg-green-400" : ""
+                            }`}
+                            onClick={() => selectCorrect(answer)}
+                        >
+                            <CardHeader>
+                                <CardDescription>Choice {answer}</CardDescription>
+                                <CardTitle>{answer}</CardTitle>
+                            </CardHeader>
+                        </Card>
+                    ))}
+            </div>
         </div>
     );
 }
