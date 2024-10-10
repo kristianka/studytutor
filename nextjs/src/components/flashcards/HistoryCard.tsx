1ae19836ab4f9520b99ea95b2ac7d34cfba45c57
Kristian <49764796+kristianka@users.noreply.github.com>
2024-10-10 22:42:26 +0300

    add loading skeletons to history

--- nextjs/src/components/flashcards/HistoryCard.tsx
+++ nextjs/src/components/flashcards/HistoryCard.tsx
@@ -3,12 +3,12 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { User } from "@/types";
 import { useHistory } from "@/lib/history";
 import { HistoryCardContent } from "./HistoryCardContent";
-import { LoadingSpinner } from "../LoadingSpinner";
-import { SWRConfig } from "swr";
+import { HistorySkeleton } from "./HistorySkeleton";
 
 export default function HistoryCard({ user }: { user: User }) {
     const { data: history, isLoading } = useHistory();
     const cleanedHistory = history ? history.filter((a) => a.role === "assistant") : [];
+    const skeletonArray = Array(3).fill(null);
 
     return (
         <Card className="w-auto">
@@ -17,7 +17,7 @@ export default function HistoryCard({ user }: { user: User }) {
             </CardHeader>
             {/* remember to sort by newest first */}
             <CardContent className="space-y-2">
-                {isLoading && <LoadingSpinner />}
+                {isLoading && skeletonArray.map((_, index) => <HistorySkeleton key={index} />)}
                 {cleanedHistory &&
                     cleanedHistory.map((h) => (
                         <HistoryCardContent key={h.content} history={h} user={user} />
