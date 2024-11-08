dd680af774152e5bbf1cdbf96fe2b49e9da19c73
Henri Sjöblom <henri.sjoblom@outlook.com>
2024-11-08 21:48:26 +0200

    settings tests updated

--- nextjs/tests/settings.spec.ts
+++ nextjs/tests/settings.spec.ts
@@ -1,53 +1,49 @@
-{
-    /*
 import { test, expect } from "@playwright/test";
 import { register, login } from "../test-helpers/misc";
 
-test.describe("authentication and authorization", async () => {
-    test.beforeEach(async () => {
-        await fetch("http://localhost:3000/api/reset", { method: "GET" });
-    });
-
+test.describe("settings page", async () => {
     test("can change profile information", async ({ page }) => {
-        await register(page, "John", "Doe", "test@profile.org", "password123!");
-        await page.goto("http://localhost:3000/logout");
-        await login(page, "test@profile.org", "password123!");
+        // reset db
+        await fetch("http://localhost:3000/api/reset", { method: "GET" });
 
+        await register(page, "John", "Doe", "test@profile.org", "password123!");
         await page.goto("http://localhost:3000/settings");
         await expect(page).toHaveTitle("Settings - Study Tutor");
 
-        await expect(page.getByLabel("First Name")).toBeVisible();
-        await expect(page.getByLabel("Last Name")).toBeVisible();
-        await expect(page.getByLabel("Email")).toBeVisible();
+        await expect(page.locator("text=First Name")).toBeVisible();
+        await expect(page.locator("text=Last Name")).toBeVisible();
+        await expect(page.locator("text=Email")).toBeVisible();
 
-        // Change name and email
-        await page.getByLabel("First Name").fill("Mike");
-        await page.getByLabel("Last Name").fill("Sullivan");
-        await page.getByLabel("Email").fill("mike@sullivan.com");
+        //Change name and email
+        await page.fill('input[name="firstName"]', "Mike");
+        await page.fill('input[name="lastName"]', "Sullivan");
+        await page.fill('input[name="email"]', "mike@sullivan.com");
 
         await page.getByRole("button", { name: "Update Profile" }).click();
 
         //Check name and email are updated
-        //await expect(page.getByLabel("First Name")).toHaveText("Mike");
-        //await expect(page.getByLabel("Last Name")).toHaveText("Sullivan");
-        //await expect(page.getByLabel("Email")).toHaveText("mike@sullivan.com");
+        await expect(page.locator('input[name="firstName"]')).toHaveValue("Mike");
+        await expect(page.locator('input[name="lastName"]')).toHaveValue("Sullivan");
+        await expect(page.locator('input[name="email"]')).toHaveValue("mike@sullivan.com");
     });
+});
 
+test.describe("settings page", async () => {
     test("can change password", async ({ page }) => {
-        await register(page, "John", "Doe", "test@change.org", "password123!");
-        await page.goto("http://localhost:3000/logout");
-        await login(page, "test@change.org", "password123!");
+        // reset db
+        await fetch("http://localhost:3000/api/reset", { method: "GET" });
 
+        await register(page, "John", "Doe", "test@password.org", "password123!");
         await page.goto("http://localhost:3000/settings");
         await expect(page).toHaveTitle("Settings - Study Tutor");
 
         await page.evaluate(() => window.scrollBy(0, document.body.scrollHeight));
-        const currentPassVisible = await page.isVisible('label:has-text("Current Password")');
-        expect(currentPassVisible).toBe(true);
-        const newPassVisible = await page.isVisible('label:has-text("New Password")');
-        expect(newPassVisible).toBe(true);
-        const confirmPassVisible = await page.isVisible('label:has-text("Confirm Password")');
-        expect(confirmPassVisible).toBe(true);
+
+        await expect(page.locator("text=Current Password")).toBeVisible();
+        await expect(page.locator("text=New Password")).toBeVisible();
+        await expect(page.locator("text=Confirm Password")).toBeVisible();
+
+        //Change password
         await page.fill('input[name="currentPassword"]', "password123!");
         await page.fill('input[name="newPassword"]', "newp123!");
         await page.fill('input[name="confirmedPassword"]', "newp123!");
@@ -55,18 +51,13 @@ test.describe("authentication and authorization", async () => {
         await page.getByRole("button", { name: "Change Password" }).click();
         await page.waitForSelector('button:has-text("Change Password")');
 
-        const currentPasswordValue = await page.inputValue('input[name="currentPassword"]');
-        expect(currentPasswordValue).toBe("");
-        const newPasswordValue = await page.inputValue('input[name="newPassword"]');
-        expect(newPasswordValue).toBe("");
-        const confirmPasswordValue = await page.inputValue('input[name="confirmedPassword"]');
-        expect(confirmPasswordValue).toBe("");
+        //Check passwords fields are cleared
+        await expect(page.locator('input[name="currentPassword"]')).toHaveValue("");
+        await expect(page.locator('input[name="newPassword"]')).toHaveValue("");
+        await expect(page.locator('input[name="confirmedPassword"]')).toHaveValue("");
 
+        //Logout and login with new password
         await page.goto("http://localhost:3000/logout");
-        await login(page, "test@bruhmail.org", "newp123!");
+        await login(page, "test@password.org", "newp123!");
     });
-
 });
-
-*/
-}
