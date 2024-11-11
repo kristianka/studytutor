089dc01568bdbca30e3fcaefad58d9c2fd7ec2ec
Kristian <49764796+kristianka@users.noreply.github.com>
2024-11-12 00:54:22 +0200

    clean up code

--- nextjs/src/app/api/cards/route.ts
+++ nextjs/src/app/api/cards/route.ts
@@ -1,20 +1,7 @@
 "use server";
 import { createServiceRoleClient } from "@/utils/supabase/server";
-import { OpenAI } from "openai";
 import { NextResponse } from "next/server";
-
-async function initClients() {
-    try {
-        const supabase = createServiceRoleClient();
-        const openai = new OpenAI({
-            apiKey: process.env.OPENAI_API_KEY
-        });
-        return { supabase, openai };
-    } catch (error) {
-        console.error("Error initializing clients:", error);
-        throw new Error("Failed to initialize clients");
-    }
-}
+import initClients from "@/lib/initClient";
 
 // retrieve thread history
 async function getThreadHistory(
--- nextjs/src/app/api/profile/cards/route.ts
+++ nextjs/src/app/api/profile/cards/route.ts
@@ -1,21 +1,8 @@
 "use server";
 import { createServiceRoleClient } from "@/utils/supabase/server";
-import { OpenAI } from "openai";
 import { NextResponse } from "next/server";
 import { CardsDifficultyType } from "@/types";
-
-async function initClients() {
-    try {
-        const supabase = createServiceRoleClient();
-        const openai = new OpenAI({
-            apiKey: process.env.OPENAI_API_KEY
-        });
-        return { supabase, openai };
-    } catch (error) {
-        console.error("Error initializing clients:", error);
-        throw new Error("Failed to initialize clients");
-    }
-}
+import initClients from "@/lib/initClient";
 
 async function updateCards(
     supabase: ReturnType<typeof createServiceRoleClient>,
